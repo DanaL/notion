c53081f15ca35674429c178f3e138f3abf16ba91
Dana <dana@lrsd.net>
2018-10-15 10:05:30 -0500

    Wrote a version of n_strcpy that'll compile on Windows using MingW/gcc

--- util.c
+++ util.c
@@ -5,9 +5,19 @@
 #include "util.h"
 
 char* n_strcpy(char *s, char *ct) {
-  int sz = strlen(ct) + 1;
-  s = malloc(sz);
-  strlcpy(s, ct, sz);
 
-  return s;
+#ifdef _WIN32
+    /* MingW on Windows didn't include strlcpy (which may be a BSDland thing?),
+        and I read so posts pooh-poohing strncpy so I'll just go with this. */
+    int sz = strlen(ct);
+    s = malloc(sz + 1);
+    memcpy(s, ct, sz);
+    s[sz] = '\0';
+#else
+    int sz = strlen(ct) + 1;
+    s = malloc(sz);
+    strlcpy(s, ct, sz);
+#endif
+
+    return s;
 }
