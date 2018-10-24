77d36c35d53f07150e3217527dd51d6f677f4dfc
Dana Larose <ywg.dana@gmail.com>
2018-10-24 08:11:14 -0500

    Started chapter 6

--- schemer.scm
+++ schemer.scm
@@ -276,3 +276,12 @@
         ((or (atom? s1) (atom? s2) ) #f)
         (else (eqlist? s1 s2))
 )))
+
+(define numbered? (lambda (aexp)
+    (cond
+        ((atom? aexp) (number? aexp)  )
+        ((eq? (car (cdr aexp)) (quote +)) (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))  )
+        ((eq? (car (cdr aexp)) (quote *)) (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))   )
+        ((eq? (car (cdr aexp)) (quote /)) (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))   )
+    )
+))
